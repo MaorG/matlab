108e83535a635eb4643f9f3264a94479d4ee1e2e
MaorG <maor.grinberg@mail.huji.ac.il>
2017-12-12 11:43:12 +0200

    new playground script

--- playground/init.m
+++ playground/init.m
@@ -1,5 +1,6 @@
 addpath([pwd, '\..\ndtable']);
 addpath([pwd, '\..\ndtableDisplay']);
 addpath([pwd, '\..\segmentation\']);
+addpath([pwd, '\..\imagefuncs\']);
 addpath([pwd, '\..\input\']);
 addpath([pwd, '\..\addons\']);
